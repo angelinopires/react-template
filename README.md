c3fa6bf53e9b691064620234dc26abf00ff555ea
angelinopires <lino.pires@live.com>
2019-01-30 12:29:22 -0200

    Environment ready

---
+++ README.md
@@ -0,0 +1,71 @@
+# React workflow with Webpack 4
+
+This workflow was created based on several tutorials and features on internet. Enjoy!
+
+## Stack:
+
+- Latest React features. (Hooks API, Context, etc);
+- Webpack 4. Much easier to deal with;
+- SASS;
+
+## Requirements
+
+- Node > v10.x;
+- NPM > v6.4;
+- Or you can use Yarn > v1.13;
+
+## Setup:
+
+1. Clone the Project
+2. Use ``cd workflow-react``
+3. npm install or yarn install
+4. npm start or yarn start
+
+Após clonar e se posicionar na pasta do projeto:
+
+* Utilize um dos dois:
+
+``npm install ou yarn install``
+``npm start ou yarn start``
+
+## Commands:
+
+``npm start - Runs a server on developer mode``
+`npm run build - Minify and prepare your project to deployment`
+
+
+## Folder Struct:
+
+```
+├── .babelrc
+├── .gitignore
+├── manifest.webmanifest
+|── package-lock.json
+├── package.json
+├── postcss.config.js
+├── README.md
+├── webpack.config.js
+├── yarn.lock
+├── node_modules
+├── dist
+│   ├── favicon.ico
+|   ├── fonts
+|   ├── img
+│   ├── index.html
+│   ├── main.css
+│   └── main.bundle.js
+└── src
+    ├── components
+    ├── fonts
+    ├── img
+    ├── sass
+    |   └── components
+    ├── index.html
+    └── main.js
+```
+
+## Enjoy!
+
+<div style="text-align: center">
+    <img src="https://media.giphy.com/media/pa37AAGzKXoek/giphy.gif" width="500" height="350">
+</div>
---
+++ webpack.config.js
@@ -0,0 +1,123 @@
+const webpack = require('webpack');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin")
+
+module.exports = {
+  entry: {
+    main: "./src/main.js"
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve("dist")
+  },
+
+  watchOptions: {
+    ignored: "node_modules"
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      },
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            cacheDirectory: true
+          }
+        }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: '[name].[ext]',
+            outputPath: 'img/'
+          }
+        }
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [{
+          loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'fonts/'
+            }
+        }]
+      }  
+    ]
+  },
+
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+
+  devServer: {
+    contentBase: [path.join(__dirname, 'dist')],
+    clientLogLevel: 'none',
+    compress: true,
+    hot: true
+  },
+
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        sourceMap: true
+      }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    }),
+    new webpack.LoaderOptionsPlugin({
+      options: {
+        postcss: [
+            autoprefixer()
+        ]
+      }
+    }),
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new webpack.HotModuleReplacementPlugin()
+  ]
+}
