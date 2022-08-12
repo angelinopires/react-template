7d05925f4e3f516b5ffad804ee79b16ba4b53506
Angelino Oliveira <lino.pires@@live.com>
2022-08-12 14:21:25 -0300

    Add ESLint, react-router-dom

---
+++ src/index.jsx
@@ -0,0 +1,18 @@
+import * as React from 'react'
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+
+import App from './App'
+import './shared/scss/main.scss'
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root'),
+)
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
+)
